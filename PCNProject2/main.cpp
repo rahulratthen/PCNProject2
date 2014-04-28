c7194eddf73268bb8b8b2a2ace488539679ca508
rahulratthen <rahulratthen.theguy@gmail.com>
2014-04-27 21:05:54 -0500

    Version 2
    Queues don’t have negative(Good). Need to add calculations for graph

--- PCNProject2/main.cpp
+++ PCNProject2/main.cpp
@@ -23,7 +23,7 @@ int main()
 {
     using namespace std;
     EventList Elist;                // Create event list
-    enum {ARR,DEP1,DEP2,DEP3,TRANSFER11,TRANSFER13,TRANSFER23,TRANSFER32};                 // Define the event types
+    enum {ARR,DEP1,DEP2,DEP3,TRANSFER11,TRANSFER13,TRANSFER23,TRANSFER32,TDEP1,TDEP2,TDEP3};                 // Define the event types
     enum{Q1,Q2,Q3};
     
     double lambda=1;            // Arrival rate
@@ -31,10 +31,7 @@ int main()
     double mu2 = 25.0;
     double mu3 = 30.0;
     
-    int n1 = 0;
-    int n2=0;
-    int n3=0;
-    exp_rv(lambda);
+        exp_rv(lambda);
     double rs1 = (double)1/4;
     double rs2 = (double)3/4;
     double r11 = (double)1/2,r13 = (double)1/2,r3d = (double)2/5,r32 =(double)3/5;
@@ -46,13 +43,22 @@ int main()
     
     Event* CurrentEvent;
     
-    if(randomGenerator() <= rs1)
-        Elist.insert(exp_rv(lambda),ARR,Q1);
-    else
-        Elist.insert(exp_rv(lambda),ARR,Q2);
     
     for(lambda = 1; lambda <= 10 ; lambda++ )
     {
+        
+        int n1 = 0;
+        int n2=0;
+        int n3=0;
+        int tn1 = 0;
+        int tn2=0;
+        int tn3=0;
+
+        if(randomGenerator() <= rs1)
+            Elist.insert(exp_rv(lambda),ARR,Q1);
+        else
+            Elist.insert(exp_rv(lambda),ARR,Q2);
+
         double clock = 0.0;             // System clock
         Ndep = 0;
         double EN1 = 0.0, EN2 = 0.0, EN3 = 0.0;
@@ -63,9 +69,7 @@ int main()
             double prev = clock;
             clock=CurrentEvent->time;
             
-            EN1 += n1*(clock-prev);
-            EN2 += n2*(clock-prev);
-            EN3 += n3*(clock-prev);
+            
             
             switch (CurrentEvent->type)
             {
@@ -75,9 +79,9 @@ int main()
                     n1++;
                     //generate next arrival
                     if(randomGenerator() <= rs1)
-                        Elist.insert(exp_rv(lambda),ARR, Q1);
+                        Elist.insert(clock+exp_rv(lambda),ARR, Q1);
                     else
-                        Elist.insert(exp_rv(lambda),ARR, Q2);
+                        Elist.insert(clock+exp_rv(lambda),ARR, Q2);
                 
                     if (n1==1)
                     {
@@ -89,23 +93,16 @@ int main()
                     n2++;
                     //generate next arrival
                     if(randomGenerator() <= rs1)
-                        Elist.insert(exp_rv(lambda),ARR, Q1);
+                        Elist.insert(clock+exp_rv(lambda),ARR, Q1);
                     else
-                        Elist.insert(exp_rv(lambda),ARR, Q2);
+                        Elist.insert(clock+exp_rv(lambda),ARR, Q2);
                     
                     if (n2==1)
                     {
                         Elist.insert(clock+exp_rv(mu2),DEP2, Q2);
                     }
                 }
-                else
-                {
-                    n3++;
-                    if (n3==1)
-                    {
-                        Elist.insert(clock+exp_rv(mu3),DEP3, Q3);
-                    }
-                }
+                
                 break;
                 
                 case DEP2:
@@ -118,78 +115,81 @@ int main()
                 Elist.insert(clock+exp_rv(mu2),TRANSFER23, Q3);
                 break;
                 
-                case DEP3:
-                n3--;
-                if (n3 > 0)
-                {
-                    Elist.insert(clock+exp_rv(mu3),DEP3,Q3);
-                }
                 
-                
-                if(randomGenerator()<=r3d)
-                {
-                    //Exits the system
-                    Ndep++;
-                    n3--;
-                }
-                else
-                {
-                    Elist.insert(clock+exp_rv(mu3),TRANSFER32,Q2);
-                }
                 
                 break;
                 
                 
                 case TRANSFER11:
-                if(randomGenerator()<=r11)
-                {
-                    //Its going back to the same queue. Don't generate new arrival event. generate transfer event
-                    Elist.insert(clock+exp_rv(mu1),TRANSFER11,Q1);
+                    tn1++;
+                    if(tn1>0)
+                    {
+                        //Generate TDEP for this transfer event
+                        Elist.insert(clock+exp_rv(mu1), TDEP1, Q1);
+                    }
+                    break;
+                
                     
-                }
-                else
-                {
-                    Elist.insert(clock+exp_rv(mu1),TRANSFER13,Q3);
-                }
-                
-                
+                case TDEP1:
+                    tn1--;
+                    if(randomGenerator()<=r11)
+                    {
+                    //Its going back to the same queue. Don't generate new arrival event. generate transfer event
+                        Elist.insert(clock+exp_rv(mu1),TRANSFER11,Q1);
+                    }
+                    else
+                    {
+                        Elist.insert(clock+exp_rv(mu1),TRANSFER13,Q3);
+                    
+                    }
                 break;
                 
+                    
                 case TRANSFER13:
-                n1--;
-                n3++;
-                if(randomGenerator()<=r3d)
-                {
-                    //Exits the system
-                    Ndep++;
-                    n3--;
-                }
-                else
-                {
-                    Elist.insert(clock+exp_rv(mu3),TRANSFER32,Q2);
-                }
-                break;
+                    tn3++;
+                    if(tn3>0)
+                    {
+                        //Generate TDEP for this transfer event
+                        Elist.insert(clock+exp_rv(mu3), TDEP3, Q3);
+                    }
+                    break;
+                    
+                case TDEP3:
+                    tn3--;
+                    if(randomGenerator()<=r3d)
+                    {
+                        //Exits the system
+                        Ndep++;
+                    }
+                    else
+                    {
+                        Elist.insert(clock+exp_rv(mu3),TRANSFER32,Q2);
+                    }
+                    break;
                 
                 case TRANSFER23:
-                n2--;
-                n3++;
-                if(randomGenerator()<=r3d)
-                {
-                    //Exits the system
-                    Ndep++;
-                    n3--;
-                }
-                else
-                {
-                    Elist.insert(clock+exp_rv(mu3),TRANSFER32,Q2);
-                }
-                break;
-                
+                    tn3++;
+                    if(tn3>0)
+                    {
+                        //Generate TDEP for this transfer event
+                        Elist.insert(clock+exp_rv(mu3), TDEP3, Q3);
+                    }
+                    break;
+                    
+                    
                 case TRANSFER32:
-                n3--;
-                n2++;
-                Elist.insert(clock+exp_rv(mu2),TRANSFER23, Q3);
-                break;
+                    tn2++;
+                    if(tn2>0)
+                    {
+                        //Generate TDEP for this transfer event
+                        Elist.insert(clock+exp_rv(mu2), TDEP2, Q2);
+                    }
+                    break;
+                    
+                case TDEP2:
+                    tn2--;
+                    Elist.insert(clock+exp_rv(mu2),TRANSFER23, Q3);
+                    break;
                 
                 case DEP1:
                     n1--;
@@ -198,17 +198,16 @@ int main()
                         Elist.insert(clock+exp_rv(mu1),DEP1,Q1);
                     }
                 
-                    double dep_time = clock+exp_rv(mu1);
-                
+                    
                     if(randomGenerator()<=r11)
                     {
                     //Its going back to the same queue. Don't generate new arrival event. generate transfer event
-                        Elist.insert(dep_time,TRANSFER11,Q1);
+                        Elist.insert(clock+exp_rv(mu1),TRANSFER11,Q1);
                     
                     }
                     else
                     {
-                        Elist.insert(dep_time,TRANSFER13,Q3);
+                        Elist.insert(clock+exp_rv(mu1),TRANSFER13,Q3);
                     }
                     break;
                 
@@ -217,14 +216,19 @@ int main()
                 
                 
                 }
-            
+            EN1 += (n1+tn1)*(clock-prev);
+            EN2 += (n2+tn2)*(clock-prev);
+            EN3 += (n3+tn3)*(clock-prev);
         
         
-            
-            cout<<"N1 : "<<n1<<"    N2 : "<<n2<<"    N3 : "<<n3<<"    Ndep : "<<Ndep<<endl;
+            //cout<<CurrentEvent->type<<endl;
+            //cout<<"N1 : "<<n1+tn1<<"    N2 : "<<n2+tn2<<"    N3 : "<<n3+tn3<<"    Ndep : "<<Ndep<<endl;
+            cout<<"EN1 : "<<EN1/clock<<"    EN2 : "<<EN2/clock<<"    EN3 : "<<EN3/clock<<"    Ndep : "<<Ndep<<endl;
         delete CurrentEvent;
         if (Ndep > 1000) done=1;        // End condition
     }
+        Elist.clear();
         
 }
+    
 }
